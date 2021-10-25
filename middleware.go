3a2c7fa0000fe8f1a530e3b0d93dfae48cfde77a
aliforever <alipadida@live.com>
2021-10-25 18:48:54 +0330

    changes

--- middleware.go
+++ middleware.go
@@ -9,8 +9,8 @@ type XHTTPMethodOverrideHandler struct {
 	logger     Logger
 }
 
-func NewXHTTPMethodOverrideHandler(subHandler http.Handler, logger Logger) (xhttp *XHTTPMethodOverrideHandler) {
-	xhttp = &XHTTPMethodOverrideHandler{
+func NewXHTTPMethodOverrideHandler(subHandler http.Handler, logger Logger) (xhttp XHTTPMethodOverrideHandler) {
+	xhttp = XHTTPMethodOverrideHandler{
 		SubHandler: subHandler,
 		logger:     logger,
 	}
@@ -19,9 +19,10 @@ func NewXHTTPMethodOverrideHandler(subHandler http.Handler, logger Logger) (xhtt
 
 func (xhm XHTTPMethodOverrideHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	subHandler := xhm.SubHandler
+	logger := xhm.logger
 	if nil == subHandler {
-		if xhm.logger != nil {
-			xhm.logger.Error("subhandler is not passed")
+		if logger != nil {
+			logger.Error("subhandler is not passed")
 		}
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
